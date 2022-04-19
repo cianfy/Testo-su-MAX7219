39236565310c4b873a8c19d2bf734abb0af04dfc
cianfy <66866258+cianfy@users.noreply.github.com>
2022-04-19 20:37:54 +0200

    Aggiorna pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+max7219_matrix.setup(
+4,
+DigitalPin.P16,
+DigitalPin.P15,
+DigitalPin.P14,
+DigitalPin.P13
+)
+max7219_matrix.for_4_in_1_modules(
+rotation_direction.clockwise,
+false
+)
+basic.forever(function () {
+    max7219_matrix.scrollText(
+    "Hello world!",
+    75,
+    500
+    )
+})
